198e3cbdb26f2b14bad013612f6ec0aee6ad887d
Sunil Shelke <119919644+SharpSunil@users.noreply.github.com>
2024-08-28 20:49:24 +0530

    fixed

--- app/digitalmarketing/page.js
+++ app/digitalmarketing/page.js
@@ -125,6 +125,8 @@ const page = () => {
       setEmail("")
     } catch (err) {}
   };
+
+
   return (
     <>
       <div className="service-hero-parent parent">
@@ -184,12 +186,13 @@ const page = () => {
               <div className="front-image bg-img-cover"></div>
             </div>
             <div className="right">
-              {serviceDetails.map((item) => (
+              {serviceDetails.map((item,index) => (
                 <div
                   className="content"
                   data-aos="fade-up"
                   data-aos-delay="600"
                   data-aos-duration="1000"
+                  key={index}
                 >
                   <div className="left">{item.srNo}</div>
                   <div className="right">
