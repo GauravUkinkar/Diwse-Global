fe0d4c916d80d6635ecbb8d0c06ce19d467b0786
Rishabh Khade <89901588+rishabhkhade@users.noreply.github.com>
2024-09-07 18:13:25 +0530

    full form

--- app/digitalmarketing/page.js
+++ app/digitalmarketing/page.js
@@ -83,7 +83,7 @@ const page = () => {
 
   const faqContent = [
     {
-      title: " SEO or SEM",
+      title: " Search Engine Optimization (SEO) or Search Engine Marketing (SEM)",
       content:
         "With a presence in India, the UK, and UAE, we have the experience and expertise to help your business succeed in diverse markets.",
     },
